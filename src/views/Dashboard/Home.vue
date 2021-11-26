6a5d8af63e6d1d8e970d4a449b0c4b8e16d07aa5
Temitope3665 <aroyewontope@gmail.com>
2021-11-26 17:37:42 +0100

    mobile responsiveness fixed

--- src/views/Dashboard/Home.vue
+++ src/views/Dashboard/Home.vue
@@ -16,8 +16,8 @@
                 <p class="dashboard-academy-stage">All entries so far</p>
             </div>
             <div class="dashboard-academy-wrapper">
-                <p class="dashboard-app-text">Current Applications</p>
-                <h3 class="dashboard-app-no">233</h3>
+                <p class="dashboard-app-text">Academy's</p>
+                <h3 class="dashboard-app-no">4</h3>
                 <img class="mt-0 p-0" src="../../assets/icons/so-far-level.svg" alt="current" />
                 <p class="dashboard-academy-stage">So far</p>
             </div>
@@ -25,34 +25,34 @@
 
         <div class="dashboard-information mb-5">
             <div class="">
-                <p class="dashboard-history-text">History</p>
-                <p class="dashboard-update-date">Last Update  <span>18:24, 22/02/19</span></p>
+                <p class="dashboard-history-text text-md-start text-center">History</p>
+                <p class="dashboard-update-date text-md-start text-center">Last Update  <span>18:24, 22/02/19</span></p>
 
                 <div class="dashboard-info-table">
-                    <div class="d-flex flex-row">
-                        <p class="me-4 mt-3 ms-4">Academy Batch 1</p>
-                        <p class="me-4 mt-3 ms-4">15 students</p>
-                        <p class="me-4 mt-3 ms-4">started 11/09/15</p>
+                    <div class="dashboard-table-row d-flex flex-row">
+                        <p class="dashboard-col me-4 mt-3 ms-4">Academy Batch 1</p>
+                        <p class="dashboard-col me-4 mt-3 ms-4">15 students</p>
+                        <p class="dashboard-col me-4 mt-3 ms-4">started 11/09/15</p>
                     </div>
                     <div class="dashboard-table-row d-flex flex-row">
-                        <p class="me-4 mt-3 ms-4">Academy Batch 1</p>
-                        <p class="me-4 mt-3 ms-4">15 students</p>
-                        <p class="me-4 mt-3 ms-4">started 11/09/15</p>
+                        <p class="dashboard-col me-4 mt-3 ms-4">Academy Batch 1</p>
+                        <p class="dashboard-col me-4 mt-3 ms-4">15 students</p>
+                        <p class="dashboard-col me-4 mt-3 ms-4">started 11/09/15</p>
                     </div>
                     <div class="dashboard-table-row d-flex flex-row">
-                        <p class="me-4 mt-3 ms-4">Academy Batch 1</p>
-                        <p class="me-4 mt-3 ms-4">15 students</p>
-                        <p class="me-4 mt-3 ms-4">started 11/09/15</p>
+                        <p class="dashboard-col me-4 mt-3 ms-4">Academy Batch 1</p>
+                        <p class="dashboard-col me-4 mt-3 ms-4">15 students</p>
+                        <p class="dashboard-col me-4 mt-3 ms-4">started 11/09/15</p>
                     </div>
                 </div>
             </div>
 
             <div class="dashboard-create-assessment">
                     <p class="dashboard-create-text p-2 fw-bold fs-6">Create Assessment</p>
-                    <div class="text-center mb-4">    
-                        <p class="fs-6 fw-normal pe-5 ps-5 mt-5">Create test question for an incoming academy<br/> students</p>
+                    <div class="text-center mb-4 d-flex flex-column justify-content-center">    
+                        <p class="fs-6 fw-normal pe-5 ps-5 mt-5">Create test question for an incoming academy students</p>
                         <router-link :to="{ name: 'create-application' }">
-                            <Button btnText='Create Assessment' btnStyle='btn--gray mt-2 mb-4' />
+                            <Button btnText='Create Assessment' btnStyle='btn--gray mt-2' />
                         </router-link>
                     </div>
             </div>
