10d8a86023641150c04d7e99d3f1cb1796dc0255
Oluwatimileyin Akinpelu <72369149+Olaoluwadunni@users.noreply.github.com>
2021-11-29 02:32:53 +0100

    SignIn screen implemented

---
+++ src/components/Input.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="col-12 form-input mb-4" :class="inputBoxStyle">
+        <label :for="identifier" class="mb-2 form-label text-white" :class="labelStyle">{{label}}</label>
+        <input :type="type" class="form-control text-white" :class="inputStyle" id="input-box" :name="identifier" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'formInput',
+    props: {
+        identifier: String,
+        type: String,
+        label: String,
+        labelStyle: String,
+        inputStyle: String,
+        inputBoxStyle: String
+    },
+}
+</script>
